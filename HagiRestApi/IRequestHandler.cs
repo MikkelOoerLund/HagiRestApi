2b7cf7855513c141644fb3f6f20bb33507dd0010
Mikkel Lund <mikkel00lund@hotmail.com>
2023-10-13 13:28:26 +0200

    Request handlers

---
+++ HagiRestApi/IRequestHandler.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HagiRestApi
+{
+
+    public interface IRequestHandler<TRequest>
+    {
+        public IActionResult HandleRequest(TRequest request);
+    }
+}
---
+++ HagiRestApi/RequestHandlerCollection.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HagiRestApi
+{
+    public class RequestHandlerCollection<TRequest>
+    {
+        private List<IRequestHandler<TRequest>> _requestHandlers;
+
+        public RequestHandlerCollection()
+        {
+            _requestHandlers = new List<IRequestHandler<TRequest>>();
+        }
+
+
+        public void AddRequestHandler(IRequestHandler<TRequest> requestHandler)
+        {
+            if (_requestHandlers.Contains(requestHandler)) return;
+            _requestHandlers.Add(requestHandler);
+        }
+
+
+        public void AddRequestHandlers(IEnumerable<IRequestHandler<TRequest>> requestHandlers)
+        {
+            foreach (var requestHandler in requestHandlers)
+            {
+                if (_requestHandlers.Contains(requestHandler))
+                {
+                    continue;
+                }
+
+                _requestHandlers.Add(requestHandler);
+            }
+        }
+
+
+        public IActionResult HandleRequest(TRequest request)
+        {
+            foreach (var requestHandler in _requestHandlers)
+            {
+                var response = requestHandler.HandleRequest(request);
+                if (response == null) continue;
+            }
+
+            return null;
+        }
+    }
+}
